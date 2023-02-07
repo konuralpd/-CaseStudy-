3887cd97c423f178f8857943f7278c839a93756b
Konuralp <konuralpdemirtas@gmail.com>
2023-02-07 23:11:00 +0300

    Initial Commit

---
+++ VeroCaseStudyUITests/VeroCaseStudyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  VeroCaseStudyUITestsLaunchTests.swift
+//  VeroCaseStudyUITests
+//
+//  Created by Mac on 7.02.2023.
+//
+
+import XCTest
+
+final class VeroCaseStudyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
